ae3ed9477962d4f20dcb0c567142edab8a341a0d
Terry Phelps <tgphelps50@gmail.com>
2022-09-28 08:56:51 -0400

    save

--- classes.py
+++ classes.py
@@ -1,7 +1,18 @@
 
+from typing import Any, Optional
+
+
 class Obj:
+    subtype: int
+    vars: Any
+    up: Optional[Any]
+
     def __init__(self, typ: int):
         self.typ: int = typ
+
+    def __repr__(self):
+        return f'Obj({self.typ})'
+
     #
     # An Obj is like a C tagged union. 'typ' is the tag. There will be
     # some number of other fields in the Obj, depending on 'typ':
@@ -12,3 +23,21 @@ class Obj:
     # macro -> params, body, env
     # special -> subtype
     # environment frame -> vars, up
+
+
+class C:
+    # Object types
+    TINT = 1
+    TCELL = 2
+    TSYMBOL = 3
+    TPRIMITIVE = 4
+    TFUNCTION = 5
+    TMACRO = 6
+    TSPECIAL = 7
+    TENV = 8
+
+    # Special types
+    TNIL = 1
+    TDOT = 2
+    TCPAREN = 3
+    TTRUE = 4
--- lisp.py
+++ lisp.py
@@ -1,13 +1,42 @@
 #!/usr/bin/env python
 
-from classes import Obj
+
+from typing import Optional
 from globals import g
+from classes import Obj, C
 
 
 def main() -> None:
-    # XXX temp code
-    g.nil = Obj(1)
-    print('g.nil =', g.nil)
+    g.nil = make_special(C.TNIL)
+    g.dot = make_special(C.TDOT)
+    g.cparen = make_special(C.TCPAREN)
+    g.true = make_special(C.TTRUE)
+    g.symbols = g.nil
+
+    env: Obj = make_env(g.nil, None)
+    define_constants(env)
+    define_primitivess(env)
+
+
+def make_special(n: int) -> Obj:
+    obj = Obj(C.TSPECIAL)
+    obj.subtype = n
+    return obj
+
+
+def make_env(vars: Obj, up: Optional[Obj]) -> Obj:
+    obj = Obj(C.TENV)
+    obj.vars = vars
+    obj.up = up
+    return obj
+
+
+def define_constants(env: Obj) -> None:
+    print('TODO: define_constants')
+
+
+def define_primitivess(env: Obj) -> None:
+    print('TODO: define_primitivess')
 
 
 if __name__ == '__main__':
